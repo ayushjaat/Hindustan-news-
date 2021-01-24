c1b076ff454fb45de15c399227b13c1b2c9db8d1
ayushjaat <35873445+ayushjaat@users.noreply.github.com>
2021-01-24 10:53:44 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,39 @@
+// 8850c967f70e49fe9885d3045e8dc5aa
+
+let newsAccordion = document.getElementById('newsAccordion');
+
+
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://newsapi.org/v2/top-headlines?country=in&apiKey=8850c967f70e49fe9885d3045e8dc5aa', true);
+
+xhr.onload = function() {
+  if (this.status === 200) {
+    let json = JSON.parse(this.responseText);
+    let articles = json.articles;
+    console.log(articles)
+    let newshtml = "";
+    articles.forEach(function(element, index) {
+      let news = `
+       <div class="accordion-item">
+         <h2 class="accordion-header" id="heading${index}">
+        <button class="accordion-button collasped" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
+      <b>Breaking News ${index+1}:</b>                  ${element['title']}
+         </button>
+          </h2>
+            <div id="collapse${index}" class="accordion-collapse collapse" aria-labelledby="heading${index}" data-bs-parent="#accordionExample">
+                  <div class="accordion-body card-body">
+                        ${element['content']}.<a href ="${element['url']}" target ="_blank"> Read more here</a>
+                        </div>
+                      </div>
+                    `;
+      newshtml += news;
+    })
+
+    newsAccordion.innerHTML = newshtml;
+  }
+  else {
+    console.log("some error occurs.")
+  }
+}
+xhr.send()
